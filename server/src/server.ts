a15617d9d909df5371b1fb03771acb8a2467be8c
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-13 08:59:55 -0300

    feat: add cors and express json

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  return response.json({message: 'Hello World'})
-})
+app.use(cors())
+app.use(express.json());
 
 app.listen(3333, () => console.log('Server is running on port 3333'))
