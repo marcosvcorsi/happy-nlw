45a6826725ff36c43a64ebaa4568de9f539bb879
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-13 09:55:46 -0300

    feat: add ESLint and Prettier

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 
-import './database/connection'
+import './database/connection';
 
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 
-app.listen(3333, () => console.log('Server is running on port 3333'))
+app.listen(3333, () => console.log('Server is running on port 3333'));
