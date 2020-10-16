b5ba47e8e48c23c1afd7c55456bb7430ccc805c2
Marcos Vinicius Corsi <marcosvcorsi@gmail.com>
2020-10-15 21:05:24 -0300

    feat: add navigation

---
+++ mobile/src/pages/OrphanageDetails.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const OrphanageDetails: React.FC = () => {
+  return <View><Text>Orphanage Details</Text></View>;
+}
+
+export default OrphanageDetails;
